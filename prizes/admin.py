b0ebf6cf80cd2d10ce11e6a3756afe458a45714e
Chris Cogdon <chris@cogdon.org>
2013-06-15 15:30:49 -0700

    Records IP for more actions. Cache control decorators. Better admin screens. Prize count limits. Samples page (for admins only). Disable prizes option.

--- prizes/admin.py
+++ prizes/admin.py
@@ -4,6 +4,5 @@ from models import *
 class WinnerAdmin(ModelAdmin):
     list_display = ["key","name","email","authentication_ip","authentication_time"]
     search_fields = ["key","name","email","authentication_ip","details_ip"]
-    date_hierarchy = "authentication_time"
 
 site.register(Winner,WinnerAdmin)
