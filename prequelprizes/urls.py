60e7f0bda75ac965e7a5e0facb24af1bdfefcffd
Chris Cogdon <chris@cogdon.org>
2013-06-20 22:16:55 -0700

    Removed link to 500 test, and changed admin URL

--- prequelprizes/urls.py
+++ prequelprizes/urls.py
@@ -12,16 +12,15 @@ urlpatterns = patterns('',
     # url(r'^prequelprizes/', include('prequelprizes.foo.urls')),
 
     # Uncomment the admin/doc line below to enable admin documentation:
-    url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
+    url(r'^:3_admin/doc/', include('django.contrib.admindocs.urls')),
 
     # Uncomment the next line to enable the admin:
-    url(r'^admin/', include(admin.site.urls)),
+    url(r'^:3_admin/', include(admin.site.urls)),
 
     url(r'^getkey/$', 'prizes.views.get_key'),
     url(r'^checkkey/$', 'prizes.views.check_key'),
     url(r'^details/$', 'prizes.views.enter_details'),
     url(r'^thanks/$', 'prizes.views.thanks'),
-    url(r'^guru/$', 'prizes.views.guru_error'),
     url(r'^guru/samples/$', TemplateView.as_view(template_name="prizes/samples.html")),
 )
 
