57fbd7e0b9ff2b32f8bce182a3320985b24a0620
Chris Cogdon <chris@cogdon.org>
2013-07-08 00:42:45 -0400

    add details time to display

--- prizes/admin.py
+++ prizes/admin.py
@@ -2,7 +2,7 @@ from django.contrib.admin import site, ModelAdmin
 from models import *
 
 class WinnerAdmin(ModelAdmin):
-    list_display = ["key","name","email","game_time","authentication_ip","authentication_time"]
+    list_display = ["key","name","email","game_time","authentication_ip","authentication_time","details_time"]
     search_fields = ["key","name","email","authentication_ip","details_ip"]
 
 site.register(Winner,WinnerAdmin)
